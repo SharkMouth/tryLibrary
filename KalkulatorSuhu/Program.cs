1a47450a2b3fee96a97a8607a4b0700ce13c698b
SharkMouth <40568844+SharkMouth@users.noreply.github.com>
2024-05-02 21:31:29 +0700

    first commit

---
+++ KalkulatorSuhu/Program.cs
@@ -0,0 +1,15 @@
+﻿using KonversiSuhuLibraries;
+using System;
+
+namespace KalkulatorSuhu {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan suhu dalam celsius: ");
+            double suhuC = double.Parse(Console.ReadLine());
+            double suhuF = KonversiSuhu.CelsiuskeFahrenheit(suhuC);
+            Console.WriteLine("Suhu dalam Fahrenheit: " + suhuF);
+        }
+    }
+}
---
+++ KonversiSuhuLibraries/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace KonversiSuhuLibraries
+{
+    public static class KonversiSuhu
+    {
+        public static double CelsiuskeFahrenheit(double inCelsius)
+        {
+            return (inCelsius * 9 / 5) + 32;
+        }
+
+        public static double FahrenheitkeCelsius(double inFahrenheit)
+        {
+            return (inFahrenheit - 32) * 5 / 9;
+        }
+    }
+}
